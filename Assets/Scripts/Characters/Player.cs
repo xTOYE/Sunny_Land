0cf601084fe6aa438776726328f442c5fad2b110
xTOYE <carltonrakic@gmail.com>
2019-08-30 15:51:37 +1000

    more portal

--- Assets/Scripts/Characters/Player.cs
+++ Assets/Scripts/Characters/Player.cs
@@ -10,12 +10,24 @@ public class Player : MonoBehaviour
     public float jumpHeight = 5f;
     public float climbSpeed = 10f;
     public float moveSpeed = 10f;
+    public float portalDistance = 1f; //distance player needs to be to use portal
 
     private CharacterController2D controller;
+    private Transform currentPortal; //referece to current Portal
 
     void Start()
     {
         controller = GetComponent<CharacterController2D>();
+    }
+
+    public void OnDrawGizmos()
+    {
+        // trigger if player is in radius of portal trigger
+        if (currentPortal != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(currentPortal.position, portalDistance);
+        }
     }
 
     void Update()
@@ -39,11 +51,23 @@ public class Player : MonoBehaviour
         controller.Climb(vertical * climbSpeed);
 
         controller.Move(horizontal * moveSpeed);
+
+        if (currentPortal != null)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                float distance = Vector2.Distance(transform.position, currentPortal.position);
+                if (distance < portalDistance)
+                {
+                    currentPortal.SendMessage("Interact");
+                }
+            }
+        }
     }
     private void OnTriggerEnter2D(Collider2D col)
     {
         //detect hitting items
-        if(col.gameObject.tag == "Item")
+        if (col.gameObject.tag == "Item")
         {
             //add 1 to score - gamemanager
             GameManager.Instance.AddScore(1);
@@ -51,17 +75,19 @@ public class Player : MonoBehaviour
             //destroy item
             Destroy(col.gameObject);
         }
+        //detect hitting items
+        if (col.gameObject.tag == "Portal")
+        {
+            currentPortal = col.transform;
+        }
     }
 
-    private void OnTriggerStay2D(Collider2D col)
+    private void OnTriggerExit2D(Collider2D col)
     {
-        
-        if(col.CompareTag("Portal"))
+        //detect hitting items
+        if (col.CompareTag("Portal"))
         {
-            if(Input.GetKeyDown(KeyCode.UpArrow))
-            {
-                col.SendMessage("Interact");
-            }
+            currentPortal = null;
         }
     }
 }
